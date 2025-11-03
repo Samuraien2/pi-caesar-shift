6a1103d20b333e84a737c070deb56c9a5f497d4c
Samuraien2 <samuelmjacobsen@gmail.com>
2025-11-03 15:52:01 +0100

    Made website work offline

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-// if this was an actual encryption task i would just use an actual encryption algorithm
-// but since its just for fun this will do.. :)
-// introducing... the: PI CAESAR SHIFT!!!!
+/*
+Pi Caesar Shift - https://github.com/Samuraien2/pi-caesar-shift
+Just like the original caesar shift this uses a predefined ascii-like character map.
+Only difference being that the original only contains letters.
+This algorithm works by essentially applying a pi mask on top of all the letters.
+The nth letter of the input will be shifted left/right by the nth letter of pi.
 
-const includedLetters = "!\"$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ [\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+Uses PI calculation algorithm by Andrew Jennings
+http://ajennings.net/blog/a-million-digits-of-pi-in-9-lines-of-javascript.html
+*/
 
-let piDigits = null;
-
-async function getPiDigits() {
-    const response = await fetch('https://api.pi.delivery/v1/pi?start=0&numberOfDigits=1000');
-    const data = await response.json();
-    return data.content;
-}
-
-getPiDigits().then(digits => {
-    piDigits = digits;
-    if (encryptInput.value) {
-        encrypt();
+const piGenSrc = `
+self.onmessage = e => {
+    const digits = BigInt(e.data);
+    let i = 1n;
+    let x = 3n * (10n ** (digits + 19n));
+    let pi = x;
+    while (x > 0) {
+        x = x * i / ((i + 1n) * 4n);
+        pi += x / (i + 2n);
+        i += 2n;
     }
-    if (decryptInput.value) {
-        decrypt();
-    }
-})
+    self.postMessage("" + pi / (10n ** 20n));
+}`;
+
+const piGen = new Worker(URL.createObjectURL(
+    new Blob([piGenSrc], { type: "application/javascript" })
+));
+piGen.onmessage = e => pi = e.data;
+piGen.postMessage(10000);
+
+const characterMap = "!\"$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ [\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+let pi = Math.PI.toString();
 
 // expands / shrinks textarea to fit the amount of lines in the textarea
 function fitTextarea(textarea) {
@@ -33,53 +44,42 @@ const encryptOutput = document.getElementById("encrypt-out");
 const decryptInput = document.getElementById("decrypt-in");
 const decryptOutput = document.getElementById("decrypt-out");
 
-// yes it would probably be a better practice and more modular to create a function for encrypt and decrypt...
-// but i dont want to
-
-function encrypt() {
-    const input = encryptInput.value;
+// shifts every character in a string by each PI digit
+function piCaesarShift(input, reversed) {
     let output = "";
-
     for (let i = 0; i < input.length; i++) {
         const ch = input.charAt(i);
-
-        let index = includedLetters.indexOf(ch);
+        const index = characterMap.indexOf(ch);
+        // if character is in the character map
         if (index != -1) {
-            const code = (index + parseInt(piDigits.charAt(i))) % includedLetters.length;
-            output += includedLetters.charAt(code);
+            const piDigit = parseInt(pi.charAt(i));
+            const code = reversed ? (index - piDigit) : (index + piDigit);
+            const newIndex = wrapMod(code, characterMap.length);
+            output += characterMap.charAt(newIndex);
         } else {
             output += ch;
         }
     }
+    return output;
+}
 
-    encryptOutput.value = output;
+const ENCRYPT = false;
+const DECRYPT = true;
+
+function encrypt() {
+    encryptOutput.value = piCaesarShift(encryptInput.value, ENCRYPT);
     fitTextarea(encryptInput);
     fitTextarea(encryptOutput);
 }
 
-function wrapMod(n, m) {
-    return ((n % m) + m) % m;
-}
-
 function decrypt() {
-    const input = decryptInput.value;
-    let output = "";
-
-    for (let i = 0; i < input.length; i++) {
-        let ch = input.charAt(i);
-        let index = includedLetters.indexOf(ch);
-        if (index != -1) {
-            const code = wrapMod(index - parseInt(piDigits.charAt(i)), includedLetters.length);
-            output += includedLetters.charAt(code);
-        } else {
-            output += ch;
-        }
-    }
-
-    decryptOutput.value = output;
+    decryptOutput.value = piCaesarShift(decryptInput.value, DECRYPT);
     fitTextarea(decryptInput);
     fitTextarea(decryptOutput);
 }
+
+// modulus operator, but if it goes in negative it wraps around, why is this not default javascript :(
+const wrapMod = (n, m) => ((n % m) + m) % m;
 
 encryptInput.addEventListener('input', encrypt);
 decryptInput.addEventListener('input', decrypt);
